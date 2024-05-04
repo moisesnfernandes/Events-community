f771d408bbf43b99e4177c1d337c77e56973c837
Moises Fernandes <53123458+moisesnfernandes@users.noreply.github.com>
2024-05-04 17:49:28 +0000

    formatacçao de texto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-return <h1>Obrigado por ser essa pessoa incrivel na minha vida</h1>
-};
+  return <h1>Obrigado por ser essa pessoa incrivel na minha vida</h1>;
+}
+function dave(){
+    moises a
+}
 
 export default Home;
